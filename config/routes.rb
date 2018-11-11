a2214e0ff124d7c701fa08fb508c3b33f1fceff3
Nathan Huberty <nathan.huberty@orange.fr>
2018-11-11 13:51:31 +0100

    up bt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'movies#index'
   resources :movies, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
